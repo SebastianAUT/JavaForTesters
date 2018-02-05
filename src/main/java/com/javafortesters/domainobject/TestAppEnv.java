7ac04a9ca8c7638083e49d1debb99cc736728a0e
Seba <sebastianstanicki@yahoo.pl>
2018-02-05 20:28:44 +0100

    Fixed main/java folder. Added Chapter5

---
+++ src/main/java/com/javafortesters/domainobject/TestAppEnv.java
@@ -0,0 +1,4 @@
+package com.javafortesters.domainobject;
+
+public class TestAppEnv {
+}
---
+++ src/test/java/com/javafortesters/chap005testwithourownclasses/domainobject/examples/TestAppEnvironmentTest.java
@@ -0,0 +1,12 @@
+
+package com.javafortesters.chap005testwithourownclasses.domainobject.examples;
+import com.javafortesters.domainobject.TestAppEnv;
+import	org.junit.Test;
+import	static	org.junit.Assert.assertEquals;
+
+public class TestAppEnvironmentTest {
+    @Test
+    public void canGetUrlStatically(){
+        //assertEquals("Returns Hard Coded URL","http://192.123.0.3:67", TestAppEnv.getUrl());
+    }
+}
