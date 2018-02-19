6ffcab3f44a2553b395fb378bb394e492056bb9a
Seba <sebastianstanicki@yahoo.pl>
2018-02-19 21:11:45 +0100

    Arrays lesson 10

--- src/test/java/com/javafortesters/chap010arraysclasses/examples/ArraysTest.java
+++ src/test/java/com/javafortesters/chap010arraysclasses/examples/ArraysTest.java
@@ -3,6 +3,8 @@ package com.javafortesters.chap010arraysclasses.examples;
 import com.javafortesters.domainentities.User;
 import org.junit.Test;
 
+import java.util.concurrent.SynchronousQueue;
+
 import static org.junit.Assert.assertEquals;
 
 public class ArraysTest {
@@ -13,8 +15,21 @@ public class ArraysTest {
         uzytkownik[1] = new User("login2","haslo2");
         uzytkownik[2] = new User("login3","haslo3");
 
+        for (User zmienna : uzytkownik) {
+            System.out.println(zmienna.getUsername());
+        }
+
         assertEquals("nazwausera","login1",uzytkownik[0].getUsername());
         assertEquals("nazwausera","login2",uzytkownik[1].getUsername());
         assertEquals("nazwausera","login3",uzytkownik[2].getUsername());
+    }
+
+    @Test
+    public void testpetli() {
+        String[] tablica = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        for (String zmienna : tablica) {
+            System.out.println(zmienna);
+        }
+
     }
 }
